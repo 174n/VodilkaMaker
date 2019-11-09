a68f981b4d9f38f9b7d0f22e9933760dc4c3b9a5
Ivan Alexandrov <ivan@alexandrov.online>
2019-11-09 15:43:29 +0100

    Update deploy script

--- deploy_ghpages.sh
+++ deploy_ghpages.sh
@@ -1,5 +1,8 @@
 npm run build
 git checkout master
+git add dist -f
+git commit -m "Update gh-pages"
 git subtree split --prefix dist -b gh-pages
-git push -f gh-pages origin:gh-pages
+git push -u origin gh-pages -f
 git branch -D gh-pages
+git reset --hard HEAD~1
